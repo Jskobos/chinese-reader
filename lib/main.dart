4f9f5febba1725a7cda0dd979039e1066a9216fe
Jared Kobos <jaredkobos@gmail.com>
2020-05-26 21:16:10 -0400

    Adjust styles and use FractionalBox

--- lib/main.dart
+++ lib/main.dart
@@ -157,31 +157,36 @@ class _SingleReadingViewState extends State<SingleReadingView> {
   @override
   Widget build(BuildContext context) {
     return new Dismissible(
-      key: new ValueKey(_currentPage),
-      background: Text(getPage(_currentPage)),
-      onDismissed: (DismissDirection direction) {
-        if (direction == DismissDirection.endToStart) {
-          _pageUp();
-        } else if (direction == DismissDirection.startToEnd &&
-            _currentPage >= 0) {
-          _pageDown();
-        } else {
-          return null;
-        }
-      },
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            DefaultTextStyle(
-              style: Theme.of(context).textTheme.bodyText1,
-              textAlign: TextAlign.center,
-              child: Text(getPage(_currentPage)),
-            ),
-          ],
-        ),
-      ),
-    );
+        key: new ValueKey(_currentPage),
+        onDismissed: (DismissDirection direction) {
+          if (direction == DismissDirection.endToStart) {
+            _pageUp();
+          } else if (direction == DismissDirection.startToEnd &&
+              _currentPage >= 0) {
+            _pageDown();
+          } else {
+            return null;
+          }
+        },
+        child: FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 1,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    DefaultTextStyle(
+                      style: TextStyle(fontSize: 26, color: Colors.black),
+                      textAlign: TextAlign.center,
+                      child: Text(getPage(_currentPage)),
+                    ),
+                  ],
+                ),
+              ),
+            )));
   }
 }
